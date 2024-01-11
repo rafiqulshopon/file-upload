bf233250ebffd76124e3eccaf1737a02385ee065
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-01-12 00:36:28 +0600

    update cors policy

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -12,6 +13,8 @@ mongoose
   .catch((err) => console.error('MongoDB connection error:', err));
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
