e146622ea2d2fdd0d9d6a83986195fdd4c852b64
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-01-11 22:44:30 +0600

    imlement task id based image query

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -48,4 +48,25 @@ router.post('/', upload.array('images'), async (req, res) => {
   }
 });
 
+router.get('/:taskId', async (req, res) => {
+  const { taskId } = req.params;
+
+  if (!taskId) {
+    return res.status(400).send({ message: 'TaskId is required in the URL' });
+  }
+
+  try {
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).send({ message: 'Task not found' });
+    }
+
+    const images = await Image.find({ taskId: taskId });
+    res.status(200).send(images);
+  } catch (error) {
+    console.error('Error in fetching images:', error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
 export default router;
