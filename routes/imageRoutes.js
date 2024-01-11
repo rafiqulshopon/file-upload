9855ea9df70f569d20aa15c35e6a8e382a3096ef
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-01-11 22:17:34 +0600

    implement multiple image upload

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import multer from 'multer';
-import fs from 'fs';
-import util from 'util';
 import cloudinary from '../config/cloudinaryConfig.js';
 import Image from '../models/Image.js';
 import Task from '../models/Task.js';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
-const unlinkFile = util.promisify(fs.unlink);
 
-router.post('/', upload.single('image'), async (req, res) => {
+router.post('/', upload.array('images'), async (req, res) => {
   const { taskId } = req.body;
+  const images = req.files;
 
   if (!taskId) {
     return res.status(400).send({ message: 'TaskId is required' });
@@ -23,27 +21,22 @@ router.post('/', upload.single('image'), async (req, res) => {
       return res.status(404).send({ message: 'Task not found' });
     }
 
-    if (!req.file) {
-      return res.status(400).send({ message: 'Image file is required' });
+    for (const image of images) {
+      const result = await cloudinary.uploader.upload(image.path, {
+        folder: 'image',
+      });
+
+      const newImage = new Image({
+        image_url: result.url,
+        taskId: taskId,
+      });
+      await newImage.save();
     }
 
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: 'image',
-    });
-
-    await unlinkFile(req.file.path);
-
-    const newImage = new Image({
-      image_url: result.url,
-      taskId: taskId,
-    });
-    await newImage.save();
+    res.status(201).send({ message: 'Images uploaded successfully' });
 
     res.status(201).send(newImage);
   } catch (error) {
-    await unlinkFile(req.file.path).catch((e) =>
-      console.error('Error deleting file', e)
-    );
     console.error('Error in image upload:', error);
     res.status(500).send({ message: error.message });
   }
